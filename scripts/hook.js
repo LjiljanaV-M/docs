af3b46cbe24bc3ea394c16b6f32d9812d640a0f6
Tom Smith <tom@solrr.net>
2016-10-03 23:01:53 -0600

    Pull down code on push hook

--- scripts/hook.js
+++ scripts/hook.js
@@ -1,12 +1,16 @@
 let express = require('express');
 let bodyParser = require('body-parser');
-var build = require('./metalsmith.js').build;
+let execSync = require('child_process').execSync;
+let build = require('./metalsmith.js').build;
 let app = express();
 
 app.use(bodyParser.json());
 
 app.post('/hook', (req, res) => {
   if (req.body.ref === "refs/heads/master") {
+    console.log("Grabbing latest..");
+    console.log(execSync('git pull origin').toString())
+
     console.log("Build started..");
     build();
   }
