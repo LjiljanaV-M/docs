96001456506eefc758a050376d40062d8ec54d3c
Alvaro Cabrera <pateketrueke@gmail.com>
2017-03-17 15:24:15 -0600

    Fix: deployment path

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 npm run build
-rsync -avz "${TRAVIS_BUILD_DIR:-.}/build/" root@198.199.82.7:/var/www/html
+rsync -avz "${TRAVIS_BUILD_DIR:-.}/build/" root@198.199.82.7:/root/Dev/docs/build
