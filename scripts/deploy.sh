e279c1389b60b440f73b46afe57ccea82456ac78
Alvaro Cabrera <pateketrueke@gmail.com>
2017-03-17 15:21:33 -0600

    Fix: tweaks for menu, rsync

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 npm run build
-rsync -ravz --delete-after "${TRAVIS_BUILD_DIR:-.}/build/" root@198.199.82.7:/var/www/html
+rsync -avz "${TRAVIS_BUILD_DIR:-.}/build/" root@198.199.82.7:/var/www/html
